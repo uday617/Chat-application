8b71ebb4d095839051a1bc6b87114bef698a2ad3
uday617 <udaysinghgurjar528@.com>
2025-10-28 22:00:37 +0530

    `Update ws-server.js to use process.env.PORT for port number`

--- server/ws-server.js
+++ server/ws-server.js
@@ -160,7 +160,7 @@ wss.on("connection", socket => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`✅ Server started on http://localhost:${PORT}`);
+  console.log(` Server started on port ${PORT}`);
 });
